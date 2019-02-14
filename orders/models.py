dfcbed8f7a4f4676a37565a11c7467b22e8db867
Leo <leonidshreder@gmail.com>
2019-02-14 12:05:57 +0200

    first 1

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django.db.models.signals import post_save
 
 class Status(models.Model):
     name = models.CharField(max_length=24, blank=True, null=True, default=None)
@@ -34,10 +35,10 @@ class Order(models.Model):
 
 class ProductInOrder(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True, default=None)
-    order1 = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, default=None)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, default=None)
     number = models.IntegerField(default=1)
     price_per_item = models.DecimalField(max_digits=10, decimal_places=1, default=0)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=1, default=0) #price*number
+    total_price = models.DecimalField(max_digits=10, decimal_places=1, default=0) #price*number
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
@@ -47,5 +48,35 @@ class ProductInOrder(models.Model):
 
     class Meta:
         verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
+        verbose_name_plural = 'Товары в заказе'
 
+    def save(self, *args, **kwargs):
+        price_per_item = self.product.price
+        self.price_per_item = price_per_item
+        self.total_price = self.number * self.price_per_item
+
+        order = self.order
+        all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+
+        order_total_price = 0
+        for item in all_products_in_order:
+            item.total_price += item.total_price
+        order_total_price = self.total_price
+
+        self.order.total_price = order_total_price
+        self.order.save(force_update=True)
+
+        super(ProductInOrder, self).save(*args, ** kwargs)
+
+    def product_in_order_post_save(sender, instance, created, **kwargs):
+        order = instance.order
+        all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+
+        order_total_price = 0
+        for item in all_products_in_order:
+            order_total_price += item.total_price
+
+        instance.order.total_price = order_total_price
+        instance.order.save(force_update=True)
+
+    post_save.connect(product_in_order_post_save, sender=Product)
--- products/models.py
+++ products/models.py
@@ -4,12 +4,13 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=64, blank=True, null=True, default=None)
     desription = models.TextField(blank=True, null=True, default=None)
+    price = models.DecimalField(max_digits=10, decimal_places=1, default=0)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "%s" % self.name
+        return "%s, %s" % (self.price, self.name)
 
     class Meta:
         verbose_name = 'Товар'
